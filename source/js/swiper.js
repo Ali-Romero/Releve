bb5b05ed8d4a9b69f7d62b34187230521dcf574f
Ali-Romero <sherali920825@yandex.ru>
2023-07-27 23:16:06 +0600

    support section

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -31,6 +31,39 @@ function initSwiperFinance() {
   })
 }
 
+function initSwiperSupport() {
+  const swiper = new Swiper('.swiper-support', {
+    speed: 400,
+    effect: 'coverflow',
+    pagination: {
+      el: '.support-pagination',
+      type: 'progressbar',
+    },
+    breakpoints: {
+      200: {
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 300,
+          modifier: 1,
+          slideShadows: false,
+        },
+        allowTouchMove: true,
+      },
+      768: {
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 0,
+          modifier: 0,
+          slideShadows: false,
+        },
+        allowTouchMove: false,
+      },
+    },
+  })
+}
+
 function initSwiperGallery() {
   const swiper = new Swiper('.swiper-gallery', {
     speed: 400,
@@ -54,4 +87,5 @@ function initSwiperGallery() {
 $(document).ready(function () {
   initSwiperFinance()
   initSwiperGallery()
+  initSwiperSupport()
 })
